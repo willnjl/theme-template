299c2d486413b62b9055b04386c8bf9ba6304535
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-01-29 17:41:48 +0000

    init

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require("gulp");
+const { task, series, watch, dest } = require("gulp");
+const browserSync = require("browser-sync");
+const reload = browserSync.reload;
+const autoprefixer = require("gulp-autoprefixer");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
+
+// Local site URL //////////////////////////////////
+const localUrl = "http://1510-startertheme.local/";
+
+let localOptions = {
+  proxy: localUrl,
+};
+
+let staticOptions = {
+  server: {
+    baseDir: "./",
+  },
+};
+
+///////////////.//////////////////////////////////
+
+task("browser-sync", (cb) => {
+  browserSync.init(localOptions);
+
+  cb();
+});
+
+task("clean-css", () => {
+  return gulp
+    .src("./css/style.css")
+    .pipe(autoprefixer())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(dest("./css/"));
+});
+
+let browserReload = (cb) => {
+  reload();
+  cb();
+};
+
+task("watcher", () => {
+  watch("./**/*.php", browserReload);
+  watch("./**/*.js", browserReload);
+  watch("./css/style.css", series("clean-css", browserReload));
+});
+
+task("default", series("browser-sync", "watcher"));
---
+++ search-filter/results.php
@@ -0,0 +1,75 @@
+        
+<?php
+/**
+ * Search & Filter Pro 
+ *
+ * Sample Results Template
+ * 
+ * @package   Search_Filter
+ * @author    Ross Morsali
+ * @link      https://searchandfilter.com
+ * @copyright 2018 Search & Filter
+ * 
+ * Note: these templates are not full page templates, rather 
+ * just an encaspulation of the your results loop which should
+ * be inserted in to other pages by using a shortcode - think 
+ * of it as a template part
+ * 
+ * This template is an absolute base example showing you what
+ * you can do, for more customisation see the WordPress docs 
+ * and using template tags - 
+ * 
+ * http://codex.wordpress.org/Template_Tags
+ *
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+get_header();
+?>
+
+<section class="">
+    <div class="">
+        <?php
+if ( $query->have_posts() ) :?>
+				<?php while($query->have_posts()): $query->the_post(); ?>
+					<!-- 
+						Content here 
+					-->
+				<?php endwhile; wp_reset_query() ?>
+			</div>
+		</section>  
+	Page <?php echo $query->query['paged']; ?> of <?php echo $query->max_num_pages; ?><br />
+	
+	<div class="pagination">
+		
+		<div class="nav-previous"><?php next_posts_link( 'Older posts', $query->max_num_pages ); ?></div>
+		<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
+		<?php
+			/* example code for using the wp_pagenavi plugin */
+			if (function_exists('wp_pagenavi'))
+			{
+				echo "<br />";
+				wp_pagenavi( array( 'query' => $query ) );
+			}
+		?>
+	</div>
+	<?php else :?>
+<h3>No Results</h3>
+
+<?php endif;?>
+        
+        	</div>
+</section>  
+
+<script>
+	let pagi = document.querySelector('.wp-pagenavi');
+	let form = document.querySelector('#blog-search');
+	pagi.addEventListener('click', () => {
+		form.scrollIntoView({behavior: 'smooth'})
+	})
+</script>
+        
+        
