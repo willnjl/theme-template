a843faaf57b5cf169ee54c1380bd7b6bb3a25b71
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-02-10 13:21:52 +0000

    homepage body

---
+++ template-parts/section-trailer.php
@@ -0,0 +1,5 @@
+<div>
+    <div class="wrap-tight">
+        <img src="<?= get_template_directory_uri(  ) ?>/images/trailer.jpeg" alt="" class="trailer">
+    </div>
+</div>
---
+++ template-parts/textblock-homepage.php
@@ -0,0 +1,7 @@
+<div class="text-block__homepage">
+    <div class="wrap-tight">
+        <p>
+            <strong>We empower our clients</strong> with an arsenal of <a href="#" class="link">graphic design</a>, <a href="#" class="link">motion productions</a>, <a href="#" class="link">web experiences</a>, strategic data and digital advice and insights. Our process is consultative, uncomplicated and competitively priced . We only work with clients once we understand them fully. <a href="#" class="link">Give us a shout</a> to find out more.
+        </p>
+    </div>
+</div>
