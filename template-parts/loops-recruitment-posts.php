db354ba76702c95c78ca9e082feb93668d62709c
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-02-15 11:47:47 +0000

    recruitment posts

---
+++ template-parts/loops-recruitment-posts.php
@@ -0,0 +1,57 @@
+<?php
+
+    $uri = get_template_directory_uri(  );
+
+?>
+
+<section class="grey-bg--top--qtr no-pad">
+ <div class="wrap-tight">
+     <ul class="loop--two-col">
+         <li>
+             <a href="<?= get_the_permalink() ?>" >
+                    <div class="hover-card">
+                        <img src="<?= $uri ?>/images/post-1.jpg" alt="" class="post-img">
+                        <p class="hover-card__link icon">Read More</p>
+                    </div>
+                    <p>
+                        <a href="<?= get_the_permalink() ?>" class="link">Analytics 4. The traditional Google Analytics is now irrelevant.</a>
+                    </p>
+                </a>
+         </li>
+         <li>
+             <a href="<?= get_the_permalink() ?>" >
+                    <div class="hover-card">
+                        <img src="<?= $uri ?>/images/post-1.jpg" alt="" class="post-img">
+                        <p class="hover-card__link icon">Read More</p>
+                    </div>
+                    <p>
+                        <a href="<?= get_the_permalink() ?>" class="link">Analytics 4. The traditional Google Analytics is now irrelevant.</a>
+                    </p>
+                </a>
+         </li>
+         <li>
+             <a href="<?= get_the_permalink() ?>" >
+                    <div class="hover-card">
+                        <img src="<?= $uri ?>/images/post-1.jpg" alt="" class="post-img">
+                        <p class="hover-card__link icon">Read More</p>
+                    </div>
+                    <p>
+                        <a href="<?= get_the_permalink() ?>" class="link">Analytics 4. The traditional Google Analytics is now irrelevant.</a>
+                    </p>
+                </a>
+         </li>
+         <li>
+             <a href="<?= get_the_permalink() ?>" >
+                    <div class="hover-card">
+                        <img src="<?= $uri ?>/images/post-1.jpg" alt="" class="post-img">
+                        <p class="hover-card__link icon">Read More</p>
+                    </div>
+                    <p>
+                        <a href="<?= get_the_permalink() ?>" class="link">Analytics 4. The traditional Google Analytics is now irrelevant.</a>
+                    </p>
+                </a>
+         </li>
+         
+     </ul>
+ </div>
+</section>
