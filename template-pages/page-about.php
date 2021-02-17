f8def2ff8714529e6e594c733cf1872eff7be45f
Fifteen Ten <fifteenten@Fifteens-MacBook-Pro.local>
2021-02-17 14:02:26 +0000

    homepage link fix and about init

---
+++ template-pages/page-about.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* Template Name: Homepage Default
+*
+* @package WordPress
+
+* @since Twenty Fourteen 1.0
+*/
+
+
+get_header();
+
+?>
+
+<main id="main">
+    <?php get_template_part( 'template-parts/banner', 'home' ) ?>
+    <div class="first">
+        <?php get_template_part( 'template-parts/section', 'panels' ) ?>
+    </div>
+    <section class="grey-bg--bottom">
+    <div class="wrap">
+        <?php get_template_part( 'template-parts/textblock', 'homepage' ) ?>
+        <?php get_template_part( 'template-parts/section', 'trailer' ) ?>
+        <?php get_template_part( 'template-parts/textblock', 'homepage--link' ) ?>
+    </div>
+    </section>
+    <div class="grey-bg">>
+        <?php get_template_part( 'template-parts/loops', 'client--homepage' ) ?>
+        <?php get_template_part( 'template-parts/loops', 'posts--swiper' ) ?>
+        <?php get_template_part( 'template-parts/form', 'small' ) ?>
+    </div>
+</main>
+
+<script>
+    let blackNav = false;
+</script>
+
+
+<?php get_footer() ?>
